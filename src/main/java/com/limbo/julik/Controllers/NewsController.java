fa27f8f4ec04d0d585ea0572b556ca46e480a90a
DyScrypt <iongirbu12@mail.ru>
2022-10-14 10:01:53 +0300

    Adding some features and pages

--- src/main/java/com/limbo/julik/Controllers/NewsController.java
+++ src/main/java/com/limbo/julik/Controllers/NewsController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 @Controller
 
 public class NewsController {
@@ -33,4 +36,43 @@ public class NewsController {
         articleRepo.save(article);
         return "redirect:/News";
     }
+
+
+    @GetMapping("/News/{id}")
+
+    public String NewsDetails(@PathVariable(value = "id") long id,  Model model){
+        Optional<Article> article = articleRepo.findById(id);
+        ArrayList<Article> res = new ArrayList<>();
+        article.ifPresent(res::add);
+        model.addAttribute("article", res);
+        return "news-details";
+    }
+
+    @GetMapping("/News/{id}/edit")
+
+    public String NewsEdit(@PathVariable(value = "id") long id,  Model model){
+        Optional<Article> article = articleRepo.findById(id);
+        ArrayList<Article> res = new ArrayList<>();
+        article.ifPresent(res::add);
+        model.addAttribute("article", res);
+        return "news-edit";
+    }
+
+    @PostMapping("/News/{id}/edit")
+    private String blogPostEdit(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String fulltext, @RequestParam int likes, Model model){
+        Article article = articleRepo.findById(id).orElseThrow();
+        article.setTitle(title);
+        article.setFulltext(fulltext);
+        article.setLikes(likes);
+        articleRepo.save(article);
+        return "redirect:/News";
+    }
+
+    @PostMapping("/News/{id}/remove")
+    private String blogPostDelete(@PathVariable(value = "id") long id, Model model){
+        Article article = articleRepo.findById(id).orElseThrow();
+        articleRepo.delete(article);
+        return "redirect:/News";
+    }
+
 }
